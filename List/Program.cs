cbbd2fb87c5781ec5842af60dc08469ac06f550b
Aram Baghdasarian <aram99551@gmail.com>
2021-10-01 19:12:24 +0400

    Add project files.

---
+++ List/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+namespace _List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _List arr = new _List(4);
+            arr.Add(45);
+            arr.Add(87);
+            arr.Add(66);
+            arr.Add(98);
+            arr.Add(12);
+            int a = arr[0];
+            Console.WriteLine(a);
+        }
+    }
+    public class _List
+    {
+        private const int _defaultCapacity = 4;
+        private int _size;
+        private int _version;
+        private int[] _items;
+        static readonly int[] _emptyArray = new int[0];
+        public _List()
+        {
+            _items = _emptyArray;
+        }
+        public _List(int capacity)
+        {
+            if (capacity == 0)
+                _items = _emptyArray;
+            else
+                _items = new int[capacity];
+        }
+        public int Capacity
+        {
+            get
+            {
+                return _items.Length;
+            }
+            set
+            {
+                if (value != _items.Length)
+                {
+                    if (value > 0)
+                    {
+                        int[] newItems = new int[value];
+                        if (_size > 0)
+                        {
+                            Array.Copy(_items, 0, newItems, 0, _size);
+                        }
+                        _items = newItems;
+                    }
+                    else
+                    {
+                        _items = _emptyArray;
+                    }
+                }
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return _size;
+            }
+        }
+        public void Add(int item)
+        {
+            if (_size == _items.Length) EnsureCapacity(_size + 1);
+            _items[_size++] = item;
+            _version++;
+        }
+        private void EnsureCapacity(int min)
+        {
+            if (_items.Length < min)
+            {
+                int newCapacity = _items.Length == 0 ? _defaultCapacity : _items.Length * 2;
+                if ((uint)newCapacity > 0X7FEFFFFF) newCapacity = 0X7FEFFFFF;
+                if (newCapacity < min) newCapacity = min;
+                Capacity = newCapacity;
+            }
+        }
+        public _List GetRange(int index, int count)
+        {
+            _List list = new _List(count);
+            Array.Copy(_items, index, list._items, 0, count);
+            list._size = count;
+            return list;
+        }
+        public int IndexOf(int item)
+        {
+            return Array.IndexOf(_items, item, 0, _size);
+        }
+        public void Insert(int index, int item)
+        {
+            if (_size == _items.Length) EnsureCapacity(_size + 1);
+            if (index < _size)
+            {
+                Array.Copy(_items, index, _items, index + 1, _size - index);
+            }
+            _items[index] = item;
+            _size++;
+            _version++;
+        }
+        public int LastIndexOf(int item, int v, int _size)
+        {
+            if (_size == 0)
+            { 
+                return -1;
+            }
+            else
+            {
+                return LastIndexOf(item, _size - 1, _size);
+            }
+        }
+        public bool Remove(int item)
+        {
+            int index = IndexOf(item);
+            if (index >= 0)
+            {
+                RemoveAt(index);
+                return true;
+            }
+            return false;
+        }
+        public void RemoveAt(int index)
+        {
+            if (index < _size)
+            {
+                Array.Copy(_items, index + 1, _items, index, _size - index);
+            }
+            _items[_size] = default(int);
+            _version++;
+        }
+        public void RemoveRange(int index, int count)
+        {
+            if (count > 0)
+            {
+                int i = _size;
+                _size -= count;
+                if (index < _size)
+                {
+                    Array.Copy(_items, index + count, _items, index, _size - index);
+                }
+                Array.Clear(_items, _size, count);
+                _version++;
+            }
+        }
+        public void Reverse(int index, int count)
+        {
+            Reverse(0, Count);
+        }
+        public int this[int i]
+        {
+            get => _items[i];
+            set => _items[i] = value;
+        }
+        public override string ToString()
+           => $"Count = {Count}";
+    }
+}
